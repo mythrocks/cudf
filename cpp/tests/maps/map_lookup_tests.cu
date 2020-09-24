a8ae541e571973d5eaddda44f3891fe35486a427
Mithun RK <mythrocks@gmail.com>
2020-09-23 17:44:44 -0700

    [map lookup] Fixed test headers.

--- cpp/tests/maps/map_lookup_tests.cu
+++ cpp/tests/maps/map_lookup_tests.cu
@@ -39,11 +39,11 @@
 #include <iterator>
 #include <memory>
 #include <rmm/device_buffer.hpp>
-#include <tests/utilities/base_fixture.hpp>
-#include <tests/utilities/column_utilities.hpp>
-#include <tests/utilities/column_wrapper.hpp>
-#include <tests/utilities/cudf_gtest.hpp>
-#include <tests/utilities/type_lists.hpp>
+#include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_utilities.hpp>
+#include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/cudf_gtest.hpp>
+#include <cudf_test/type_lists.hpp>
 #include "cudf/scalar/scalar.hpp"
 
 using vector_of_columns = std::vector<std::unique_ptr<cudf::column>>;
