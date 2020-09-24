3d49a0f8ceaef5c1b02165dcf491e7df5617da8e
Mithun RK <mythrocks@gmail.com>
2020-09-24 12:11:23 -0700

    [map lookup] Removed superfluous validity calculation.
    Also, some minor refactoring, renaming.

--- cpp/src/maps/map_lookup.cu
+++ cpp/src/maps/map_lookup.cu
@@ -31,11 +31,11 @@ namespace cudf {
 namespace {
 
 void __device__ search_each_list(size_type list_index,
-                        column_device_view input,
-                        mutable_column_device_view output,
-                        string_scalar_device_view lookup_key)
+                                 column_device_view input,
+                                 mutable_column_device_view output,
+                                 string_scalar_device_view lookup_key)
 {
-  if (input.is_null(list_index)) { // List row is null.
+  if (input.is_null(list_index)) {               // List row is null.
     output.element<size_type>(list_index) = -1;  // Not found.
     return;
   }
@@ -46,8 +46,10 @@ void __device__ search_each_list(size_type list_index,
 
   auto key_column{input.child(1).child(0)};
 
-  for (size_type list_element_index{start_index}; list_element_index < end_index; ++list_element_index) {
-    if (!key_column.is_null(list_element_index) && key_column.element<string_view>(list_element_index) == lookup_key.value()) {
+  for (size_type list_element_index{start_index}; list_element_index < end_index;
+       ++list_element_index) {
+    if (!key_column.is_null(list_element_index) &&
+        key_column.element<string_view>(list_element_index) == lookup_key.value()) {
       output.element<size_type>(list_index) = list_element_index;
       return;
     }
@@ -57,64 +59,44 @@ void __device__ search_each_list(size_type list_index,
 }
 
 template <int block_size>
-__launch_bounds__(block_size) __global__
-  void gpu_find_first(column_device_view input,
-                      mutable_column_device_view output,
-                      string_scalar_device_view lookup_key,
-                      size_type* __restrict__ output_valid_count)
+__launch_bounds__(block_size) __global__ void gpu_find_first(column_device_view input,
+                                                             mutable_column_device_view output,
+                                                             string_scalar_device_view lookup_key)
 {
   size_type i      = blockIdx.x * block_size + threadIdx.x;
   size_type stride = block_size * gridDim.x;
-  size_type warp_valid_count{0};
 
   auto active_threads = __ballot_sync(0xffffffff, i < input.size());
 
   while (i < input.size()) {
-    volatile bool output_valid{true};
     search_each_list(i, input, output, lookup_key);
-
-    // Set validity mask.
-    cudf::bitmask_type result_mask{__ballot_sync(active_threads, output_valid)};
-
-    // Write out the mask.
-    if (0 == threadIdx.x % cudf::detail::warp_size) {
-      output.set_mask_word(cudf::word_index(i), result_mask);
-      warp_valid_count += __popc(result_mask);
-    }
-
     i += stride;
     active_threads = __ballot_sync(active_threads, i < input.size());
   }
-
-  // Sum up valid_count across the whole block
-  size_type block_valid_count =
-    cudf::detail::single_lane_block_sum_reduce<block_size, 0>(warp_valid_count);
-
-  if (threadIdx.x == 0) { atomicAdd(output_valid_count, block_valid_count); }
 }
 
-size_type kernel_launcher(column_view const& input,
-                          mutable_column_view const& output,
-                          string_scalar& lookup_key,
-                          cudaStream_t stream)
+std::unique_ptr<column> get_gather_map_for_map_values(column_view const& input,
+                                                      string_scalar& lookup_key,
+                                                      rmm::mr::device_memory_resource* mr,
+                                                      cudaStream_t stream)
 {
   constexpr size_type block_size{256};
   cudf::detail::grid_1d grid{input.size(), block_size};
 
   auto input_device_view = cudf::column_device_view::create(input, stream);
-  auto output_view       = mutable_column_device_view::create(output, stream);
   auto lookup_key_device_view{get_scalar_device_view(lookup_key)};
-  rmm::device_scalar<size_type> device_valid_count{0, stream};
+  auto gather_map = make_numeric_column(
+    data_type{cudf::type_to_id<size_type>()}, input.size(), mask_state::ALL_VALID, stream, mr);
+  auto output_view = mutable_column_device_view::create(gather_map->mutable_view(), stream);
 
   gpu_find_first<block_size><<<grid.num_blocks, block_size, 0, stream>>>(
-    *input_device_view, *output_view, lookup_key_device_view, device_valid_count.data());
-
-  size_type valid_count = device_valid_count.value(stream);
+    *input_device_view, *output_view, lookup_key_device_view);
 
   CHECK_CUDA(stream);
 
-  return valid_count;
+  return std::move(gather_map);
 }
+
 }  // namespace
 
 std::unique_ptr<column> map_lookup(column_view const& map_column,
@@ -140,14 +122,7 @@ std::unique_ptr<column> map_lookup(column_view const& map_column,
   // Two-pass plan: construct gather map, and then gather() on structs_column.child(1). Plan A.
   // Can do in one pass perhaps, but that's Plan B.
 
-  auto gather_map              = make_numeric_column(data_type{cudf::type_to_id<size_type>()},
-                                        map_column.size(),
-                                        mask_state::UNINITIALIZED,
-                                        stream,
-                                        mr);
-  auto gather_map_mutable_view = gather_map->mutable_view();
-
-  auto valid_count = kernel_launcher(map_column, gather_map_mutable_view, lookup_key, stream);
+  auto gather_map = get_gather_map_for_map_values(map_column, lookup_key, mr, stream);
 
   // Gather map is now available.
 
--- cpp/tests/maps/map_lookup_tests.cu
+++ cpp/tests/maps/map_lookup_tests.cu
@@ -34,16 +34,16 @@
 #include <cudf/table/table_view.hpp>
 #include <cudf/types.hpp>
 #include <cudf/utilities/error.hpp>
-#include <functional>
-#include <initializer_list>
-#include <iterator>
-#include <memory>
-#include <rmm/device_buffer.hpp>
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/cudf_gtest.hpp>
 #include <cudf_test/type_lists.hpp>
+#include <functional>
+#include <initializer_list>
+#include <iterator>
+#include <memory>
+#include <rmm/device_buffer.hpp>
 #include "cudf/scalar/scalar.hpp"
 
 using vector_of_columns = std::vector<std::unique_ptr<cudf::column>>;
