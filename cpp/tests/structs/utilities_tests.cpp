6b580cfbbba076bb363626d33529c80cc20fd875
Mithun RK <mythrocks@gmail.com>
2021-08-26 21:37:14 -0700

    Tests for sliced, nested structs.

--- cpp/tests/structs/utilities_tests.cpp
+++ cpp/tests/structs/utilities_tests.cpp
@@ -24,7 +24,10 @@
 
 #include <cudf/detail/aggregation/aggregation.hpp>
 #include <structs/utilities.hpp>
+#include "cudf/column/column_factories.hpp"
 #include "cudf/detail/null_mask.hpp"
+#include "cudf/null_mask.hpp"
+#include "cudf/types.hpp"
 
 namespace cudf::test {
 
@@ -397,143 +400,107 @@ TYPED_TEST(TypedSuperimposeTest, NestedStruct_ChildNullable_ParentNullable)
   CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_structs_of_structs);
 }
 
+cudf::column_view slice_off_first_and_last_rows(cudf::column_view const& col)
+{
+  return cudf::slice(col, {1, col.size()-1})[0];
+}
+
+void mark_row_as_null(cudf::mutable_column_view const& col, size_type row_index)
+{
+  cudf::detail::set_null_mask(col.null_mask(), row_index, row_index+1, false);
+}
+
 TYPED_TEST(TypedSuperimposeTest, Struct_Sliced)
 {
-  // Test with a sliced Struct<Struct>. 
+  // Test with a sliced STRUCT column.
   // Ensure that superimpose_parent_nulls() produces the right results, even when the input is sliced.
 
   using T = TypeParam;
 
   auto nums_member          = make_nums_member<T>(nulls_at({3, 6}));
-  // auto lists_member         = make_lists_member<T>(nulls_at({4, 5}));
-  // outer_struct_members.push_back(structs{{nums_member, lists_member}, no_nulls()}.release());
-  auto structs_column = structs{{nums_member}, no_nulls()}.release();
+  auto lists_member         = make_lists_member<T>(nulls_at({4, 5}));
+  auto structs_column = structs{{nums_member, lists_member}, no_nulls()}.release();
 
   // Reset STRUCTs' null-mask. Mark second STRUCT row as null.
-  auto structs_view = structs_column->mutable_view();
-  auto num_rows     = structs_view.size();
-  cudf::detail::set_null_mask(structs_view.null_mask(), 1, 2, false);
+  mark_row_as_null(structs_column->mutable_view(), 1);
 
-  std::cout << "Unsliced input: " << std::endl;
-  print(structs_column->view());
+  // The null masks should now look as follows, with the STRUCT null mask *not* pushed down:
+  // STRUCT:       1111101
+  // nums_member:  0110111
+  // lists_member: 1001111
 
-  // Slice off the first and last rows.
-  auto sliced_structs = cudf::slice(structs_column->view(), {1, num_rows-1})[0];
-  std::cout << "Sliced Input: " << std::endl;
-  print(sliced_structs);
+  // Slice off the first and last rows. 
+  auto sliced_structs = slice_off_first_and_last_rows(structs_column->view());
+
+  // After slice(), the null masks will be:
+  // STRUCT:       11110
+  // nums_member:  11011
+  // lists_member: 00111
 
   auto [output, backing_buffers] = cudf::structs::detail::superimpose_parent_nulls(sliced_structs);
 
-  std::cout << "Output:" << std::endl;
-  print(output);
+  // After superimpose_parent_nulls(), the null masks should be:
+  // STRUCT:       11110
+  // nums_member:  11010
+  // lists_member: 00110
 
-  /*
-  // After superimpose_parent_nulls(), outer-struct column should not have pushed nulls to child
-  // structs. But the child struct column must push its nulls to its own children.
-  auto expected_nums_member  = make_nums_member<T>(nulls_at({0, 1, 3, 6}));
-  auto expected_lists_member = make_lists_member<T>(nulls_at({0, 1, 4, 5}));
-  auto expected_structs      = structs{{expected_nums_member, expected_lists_member}, nulls_at({0,1})};
-  auto expected_structs_of_structs = structs{{expected_structs}, null_at(1)};
+  // Construct expected columns using structs_column_wrapper, which should push the parent nulls
+  // down automatically. Then, slice() off the ends.
+  auto expected_nums = make_nums_member<T>(nulls_at({1,3,6}));
+  auto expected_lists = make_lists_member<T>(nulls_at({1,4,5}));
+  auto expected_unsliced_structs = structs{{expected_nums, expected_lists}, nulls_at({1})};
+  auto expected_structs = slice_off_first_and_last_rows(expected_unsliced_structs);
 
-  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_structs_of_structs);
-  */
-}
-
-TYPED_TEST(TypedSuperimposeTest, Struct_Unsliced)
-{
-  // Test with a sliced Struct<Struct>. 
-  // Ensure that superimpose_parent_nulls() produces the right results, even when the input is sliced.
-
-  using T = TypeParam;
-
-  auto nums_member          = make_nums_member<T>(nulls_at({3, 6}));
-  // auto lists_member         = make_lists_member<T>(nulls_at({4, 5}));
-  // outer_struct_members.push_back(structs{{nums_member, lists_member}, no_nulls()}.release());
-  auto structs_column = structs{{nums_member}, no_nulls()}.release();
-
-  // Reset STRUCTs' null-mask. Mark second STRUCT row as null.
-  auto structs_view = structs_column->mutable_view();
-  auto num_rows     = structs_view.size();
-  cudf::detail::set_null_mask(structs_view.null_mask(), 1, 2, false);
-
-  std::cout << "Unsliced input: " << std::endl;
-  print(structs_column->view());
-
-  // Slice off the first and last rows.
-  auto sliced_structs = cudf::slice(structs_column->view(), {1, num_rows-1})[0];
-  std::cout << "Sliced Input: " << std::endl;
-  print(sliced_structs);
-
-  auto [sliced_output, _] = cudf::structs::detail::superimpose_parent_nulls(sliced_structs);
-
-  std::cout << "Sliced Output:" << std::endl;
-  print(sliced_output);
-
-  auto [unsliced_output, __] = cudf::structs::detail::superimpose_parent_nulls(structs_column->view());
-
-  std::cout << "Unsliced Output:" << std::endl;
-  print(unsliced_output);
-
-  /*
-  // After superimpose_parent_nulls(), outer-struct column should not have pushed nulls to child
-  // structs. But the child struct column must push its nulls to its own children.
-  auto expected_nums_member  = make_nums_member<T>(nulls_at({0, 1, 3, 6}));
-  auto expected_lists_member = make_lists_member<T>(nulls_at({0, 1, 4, 5}));
-  auto expected_structs      = structs{{expected_nums_member, expected_lists_member}, nulls_at({0,1})};
-  auto expected_structs_of_structs = structs{{expected_structs}, null_at(1)};
-
-  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_structs_of_structs);
-  */
+  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_structs);
 }
 
 TYPED_TEST(TypedSuperimposeTest, NestedStruct_Sliced)
 {
-  // Test with a sliced Struct<Struct>. 
+  // Test with a sliced STRUCT<STRUCT> column.
   // Ensure that superimpose_parent_nulls() produces the right results, even when the input is sliced.
 
   using T = TypeParam;
 
   auto nums_member          = make_nums_member<T>(nulls_at({3, 6}));
-  // auto lists_member         = make_lists_member<T>(nulls_at({4, 5}));
-  auto outer_struct_members = std::vector<std::unique_ptr<cudf::column>>{};
-  // outer_struct_members.push_back(structs{{nums_member, lists_member}, no_nulls()}.release());
-  outer_struct_members.push_back(structs{{nums_member}, no_nulls()}.release());
+  auto lists_member         = make_lists_member<T>(nulls_at({4, 5}));
+  auto structs_column = structs{{nums_member, lists_member}, null_at(1)};
+  auto struct_structs_column = structs{{structs_column}, no_nulls()}.release();
 
-  // Reset STRUCTs' null-mask. Mark first STRUCT row as null.
-  auto structs_view = outer_struct_members.back()->mutable_view();
-  auto num_rows     = structs_view.size();
-  cudf::detail::set_null_mask(structs_view.null_mask(), 0, 1, false);
+  // Reset STRUCT<STRUCT>'s null-mask. Mark third row as null.
+  mark_row_as_null(struct_structs_column->mutable_view(), 2);
 
-  auto structs_of_structs = structs{std::move(outer_struct_members), std::vector<bool>(num_rows, true)}.release();
+  // The null masks should now look as follows, with the STRUCT<STRUCT> null mask *not* pushed down:
+  // STRUCT<STRUCT>: 1111011
+  // STRUCT:         1111101
+  // nums_member:    0110101
+  // lists_member:   1001101
 
-  // Modify STRUCT-of-STRUCT's null-mask. Mark second STRUCT row as null.
-  auto structs_of_structs_view = structs_of_structs->mutable_view();
-  cudf::detail::set_null_mask(structs_of_structs_view.null_mask(), 1, 2, false);
+  // Slice off the first and last rows. 
+  auto sliced_structs = slice_off_first_and_last_rows(struct_structs_column->view());
 
-  std::cout << "Unsliced input: " << std::endl;
-  print(structs_of_structs->view());
+  // After slice(), the null masks will be:
+  // STRUCT<STRUCT>: 11101
+  // STRUCT:         11110
+  // nums_member:    11011
+  // lists_member:   00111
 
-  // Slice off the first and last rows.
-  auto sliced_structs_of_structs = cudf::slice(structs_of_structs->view(), {1, num_rows-1})[0];
-  std::cout << "Sliced Input: " << std::endl;
-  print(sliced_structs_of_structs);
+  auto [output, backing_buffers] = cudf::structs::detail::superimpose_parent_nulls(sliced_structs);
 
-  auto [output, backing_buffers] =
-    cudf::structs::detail::superimpose_parent_nulls(sliced_structs_of_structs);
+  // After superimpose_parent_nulls(), the null masks will be:
+  // STRUCT<STRUCT>: 11101
+  // STRUCT:         11100
+  // nums_member:    11001
+  // lists_member:   00101
 
-  std::cout << "Output:" << std::endl;
-  print(output);
+  // Construct expected columns using structs_column_wrapper, which should push the parent nulls
+  // down automatically. Then, slice() off the ends.
+  auto expected_nums = make_nums_member<T>(nulls_at({3,6}));
+  auto expected_lists = make_lists_member<T>(nulls_at({4,5}));
+  auto expected_structs = structs{{expected_nums, expected_lists}, nulls_at({1})};
+  auto expected_struct_structs = structs{{expected_structs}, null_at(2)};
+  auto expected_sliced_structs = slice_off_first_and_last_rows(expected_struct_structs);
 
-  /*
-  // After superimpose_parent_nulls(), outer-struct column should not have pushed nulls to child
-  // structs. But the child struct column must push its nulls to its own children.
-  auto expected_nums_member  = make_nums_member<T>(nulls_at({0, 1, 3, 6}));
-  auto expected_lists_member = make_lists_member<T>(nulls_at({0, 1, 4, 5}));
-  auto expected_structs      = structs{{expected_nums_member, expected_lists_member}, nulls_at({0,1})};
-  auto expected_structs_of_structs = structs{{expected_structs}, null_at(1)};
-
-  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_structs_of_structs);
-  */
+  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(output, expected_sliced_structs);
 }
 
 }  // namespace cudf::test
